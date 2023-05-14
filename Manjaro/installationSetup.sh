232b68d09bcdd9c761b2653e58c960049fe0541f
shyguyCreate <107062289+shyguyCreate@users.noreply.github.com>
2023-05-14 17:52:08 -0600

    Only change modules if broadom driver

--- Manjaro/installationSetup.sh
+++ Manjaro/installationSetup.sh
@@ -1,10 +1,23 @@
-#Install downloaded packages and dependecies
-sudo pacman -U $HOME/BroadcomPackages/*[^sig] --asdeps --needed
+################ INSTALL PACKAGES AND DEPENDECIES FOR BROADCOM WIFI DRIVER ################
 
-#Remove modules and load wl for broadcom wifi driver and update dependencies
-sudo rmmod b43 ssb
-sudo modprobe wl
-sudo depmod -a
+#Test if packages from the preinstall script are in this machine
+if [[ -d $HOME/BroadcomPackages ]]
+then
+  #Install downloaded packages as dependecies
+  sudo pacman -U $HOME/BroadcomPackages/*[^sig] --asdeps --needed
+fi
+
+#Check if device has broadcom wireless network device
+if [[ $(lspci -d 14e4:) != '' ]]
+then  
+  #Remove modules, load wl module and update dependencies
+  (lsmod | grep -wq b34) && sudo rmmod b43
+  (lsmod | grep -wq ssb) && sudo rmmod ssb
+  (lsmod | grep -wq wl)  || sudo modprobe wl
+  sudo depmod -a
+fi
+
+###########################################################################################
 
 
 #Update system
--- Manjaro/preinstallation.sh
+++ Manjaro/preinstallation.sh
@@ -6,14 +6,14 @@ mkdir -p /tmp/blankdb $HOME/BroadcomPackages
 #Rename pacman CacheDir to force download of all packages and dependecies
 sudo mv $(echo ${$(pacman -v 2>/dev/null | grep Cache | awk '{print $3}')%/}) $(echo ${$(pacman -v 2>/dev/null | grep Cache | awk '{print $3}')%/}Tmp)
 
+#Print network controller to know what wifi driver to install
+lspci | grep network -i
+
 #Print kernel release to know what linux-headers to install
 uname -r
 
-#Print network controller to know what wifi driver to install (for this case broadcom)
-lspci | grep network -i
-
 #Download all packages and dependecies for broadcom wifi driver inside custom CacheDir
-sudo pacman -Syw --cachedir $HOME/BroadcomPackages --dbpath /tmp/blankdb base-devel linux-headers broadcom-wl-dkms
+sudo pacman -Syyw --cachedir $HOME/BroadcomPackages --dbpath /tmp/blankdb base-devel linux-headers broadcom-wl-dkms
 
 #Revert rename of pacman CacheDir
 sudo mv $(echo ${$(pacman -v 2>/dev/null | grep Cache | awk '{print $3}')%/}Tmp) $(echo ${$(pacman -v 2>/dev/null | grep Cache | awk '{print $3}')%/})
