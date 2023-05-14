afc0e34346c28f2e1fc9f025081741ad197d1f05
shyguyCreate <107062289+shyguyCreate@users.noreply.github.com>
2023-05-13 19:34:40 -0600

    Add pwsh installer script

--- Manjaro/installationSetup.sh
+++ Manjaro/installationSetup.sh
@@ -57,12 +57,14 @@ git config --global user.email 107062289+shyguyCreate@users.noreply.github.com
 mkdir -p $HOME/Github/gist
 #Clone git repository from this script
 git clone https://github.com/shyguyCreate/installation-Setup.git $HOME/Github/installation-Setup
+#Clone pwshInstaller script from gist
+git clone https://gist.github.com/86b8b157c90d6b2ebcb1eb98c4a701e8.git $HOME/Github/gist/pwshInstaller
 #Clone ohmyposh config from gist
 git clone https://gist.github.com/387ff25579b25bff63a6bc1a7635be27.git $HOME/Github/gist/ohmyposh
-#Clone zoomInstaller script from gist
-git clone https://gist.github.com/fdec7db1dfe9588c0c3d735d142fcf41.git $HOME/Github/gist/zoomInstaller
 #Clone Meslo NF Installer script from gist
 git clone https://gist.github.com/3174d5463d717f7d7a8c67e45cd914be.git $HOME/Github/gist/meslofontsInstaller
+#Clone zoomInstaller script from gist
+git clone https://gist.github.com/fdec7db1dfe9588c0c3d735d142fcf41.git $HOME/Github/gist/zoomInstaller
 
 
 #Get yay to install google-chrome
@@ -108,20 +110,6 @@ sudo ln -s /var/lib/snapd/snap /snap
 
 #Install vscode
 sudo snap install code --classic
-
-
-#Install powershell
-sudo snap install powershell --classic
-mkdir -p $HOME/.config/powershell
-
-#Create symbolic link of profile.ps1 to powershell profile folder
-ln -s $HOME/Github/installation-Setup/Manjaro/profile.ps1 $HOME/.config/powershell/profile.ps1
-#Create symbolic link of ohmyposh to powershell profile folder
-ln -s $HOME/Github/gist/ohmyposh/ohmyposhCustome.omp.json $HOME/.config/powershell/ohmyposhCustome.omp.json
-
-#Install Powershell modules
-pwsh -NoProfile -c "& { Install-Module -Name Terminal-Icons -Scope CurrentUser -Force }"
-pwsh -NoProfile -c "& { Install-Module -Name PSReadLine -Scope CurrentUser -Force }"
  
 
 #Install bash-language-server for bash completion inside text editors
@@ -141,9 +129,29 @@ echo "
 alias mesloUpdate='source \$HOME/Github/gist/meslofontsInstaller/meslofontsInstaller.sh'
 " >> $HOME/.zshrc
 
+
 #Install zoom from script
 source $HOME/Github/gist/zoomInstaller/zoomInstaller.sh
 #Add alias to update zoom
 echo "
 alias zoomUpdate='source \$HOME/Github/gist/zoomInstaller/zoomInstaller.sh'
 " >> $HOME/.zshrc
+
+
+#Install powershell
+source $HOME/Github/gist/pwshInstaller/pwshInstaller.sh
+#Add alias to update zoom
+echo "
+alias pwshUpdate='source \$HOME/Github/gist/pwshInstaller/pwshInstaller.sh'
+" >> $HOME/.zshrc
+
+#Create directory for pwsh profile folder
+mkdir -p $HOME/.config/powershell
+#Create symbolic link of profile.ps1 to powershell profile folder
+ln -s $HOME/Github/installation-Setup/Manjaro/profile.ps1 $HOME/.config/powershell/profile.ps1
+#Create symbolic link of ohmyposh to powershell profile folder
+ln -s $HOME/Github/gist/ohmyposh/ohmyposhCustome.omp.json $HOME/.config/powershell/ohmyposhCustome.omp.json
+
+#Install Powershell modules
+pwsh -NoProfile -c "& { Install-Module -Name Terminal-Icons -Scope CurrentUser -Force }"
+pwsh -NoProfile -c "& { Install-Module -Name PSReadLine -Scope CurrentUser -Force }"
