72f7c768330e127ca303c7e1d9188c1a43d46cac
shyguyCreate <107062289+shyguyCreate@users.noreply.github.com>
2023-05-14 16:39:41 -0600

    Uncomment lines

--- Manjaro/preinstallation.sh
+++ Manjaro/preinstallation.sh
@@ -1,21 +1,21 @@
 ################ GET ALL PACKAGES AND DEPENDECIES FOR BROADCOM WIFI DRIVER ################
 
 #Create directories for pacman cache and db
-#mkdir -p /tmp/blankdb $HOME/BroadcomPackages
+mkdir -p /tmp/blankdb $HOME/BroadcomPackages
 
 #Rename pacman CacheDir to force download of all packages and dependecies
-#sudo mv $(echo ${$(pacman -v 2>/dev/null | grep Cache | awk '{print $3}')%/}) $(echo ${$(pacman -v 2>/dev/null | grep Cache | awk '{print $3}')%/}Tmp)
+sudo mv $(echo ${$(pacman -v 2>/dev/null | grep Cache | awk '{print $3}')%/}) $(echo ${$(pacman -v 2>/dev/null | grep Cache | awk '{print $3}')%/}Tmp)
 
 #Print kernel release to know what linux-headers to install
-#uname -r
+uname -r
 
 #Print network controller to know what wifi driver to install (for this case broadcom)
-#lspci | grep network -i
+lspci | grep network -i
 
 #Download all packages and dependecies for broadcom wifi driver inside custom CacheDir
-#sudo pacman -Syw --cachedir $HOME/BroadcomPackages --dbpath /tmp/blankdb base-devel linux-headers broadcom-wl-dkms
+sudo pacman -Syw --cachedir $HOME/BroadcomPackages --dbpath /tmp/blankdb base-devel linux-headers broadcom-wl-dkms
 
 #Revert rename of pacman CacheDir
-#sudo mv $(echo ${$(pacman -v 2>/dev/null | grep Cache | awk '{print $3}')%/}Tmp) $(echo ${$(pacman -v 2>/dev/null | grep Cache | awk '{print $3}')%/})
+sudo mv $(echo ${$(pacman -v 2>/dev/null | grep Cache | awk '{print $3}')%/}Tmp) $(echo ${$(pacman -v 2>/dev/null | grep Cache | awk '{print $3}')%/})
 
 ###########################################################################################
