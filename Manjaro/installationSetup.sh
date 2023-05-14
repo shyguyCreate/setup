64a63f643fa51a5b52a903304a50b6f1415b0821
shyguyCreate <107062289+shyguyCreate@users.noreply.github.com>
2023-05-14 15:20:52 -0600

    Separate script to grab packages

--- Manjaro/installationSetup.sh
+++ Manjaro/installationSetup.sh
@@ -1,26 +1,3 @@
-################ GET ALL PACKAGES AND DEPENDECIES FOR BROADCOM WIFI DRIVER ################
-
-#Create directories for pacman cache and db
-#mkdir -p /tmp/blankdb $HOME/BroadcomPackages
-
-#Rename pacman CacheDir to force download of all packages and dependecies
-#sudo mv $(echo ${$(pacman -v 2>/dev/null | grep Cache | awk '{print $3}')%/}) $(echo ${$(pacman -v 2>/dev/null | grep Cache | awk '{print $3}')%/}Tmp)
-
-#Print kernel release to know what linux-headers to install
-#uname -r
-
-#Print network controller to know what wifi driver to install (for this case broadcom)
-#lspci | grep network -i
-
-#Download all packages and dependecies for broadcom wifi driver inside custom CacheDir
-#sudo pacman -Syw --cachedir $HOME/BroadcomPackages --dbpath /tmp/blankdb base-devel linux-headers broadcom-wl-dkms
-
-#Revert rename of pacman CacheDir
-#sudo mv $(echo ${$(pacman -v 2>/dev/null | grep Cache | awk '{print $3}')%/}Tmp) $(echo ${$(pacman -v 2>/dev/null | grep Cache | awk '{print $3}')%/})
-
-###########################################################################################
-
-
 #Install downloaded packages and dependecies
 sudo pacman -U $HOME/BroadcomPackages/*[^sig] --asdeps --needed
 
