d658b6e8e51088fa0db542cb8a41258a12d60a89
shyguyCreate <107062289+shyguyCreate@users.noreply.github.com>
2023-06-09 07:41:38 -0600

    Add pair quotes binding

--- Windows/profile.ps1
+++ Windows/profile.ps1
@@ -17,8 +17,40 @@ Set-PSReadLineOption -HistorySavePath "$PROFILE_FOLDER\ConsoleHost_history.txt"
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -EditMode Windows
-if($Env:TERM_PROGRAM -ne 'vscode'){
+
+#VSCode terminal is too small for ListView
+if($Env:TERM_PROGRAM -ne 'vscode')
+{
     Set-PSReadLineOption -PredictionViewStyle ListView
+}
+#Search for the item in the history that starts with the current input when no ListView
+if ("InlineView" -eq (Get-PSReadLineOption | Select-Object -ExpandProperty PredictionViewStyle))
+{
+    Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+    Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+}
+
+#Add key binding to insert paired quotes
+Set-PSReadLineKeyHandler -Chord '"',"'" `
+                         -BriefDescription SmartInsertQuote `
+                         -LongDescription "Insert paired quotes if not already on a quote" `
+                         -ScriptBlock {
+    param($key, $arg)
+
+    $line = $null
+    $cursor = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+
+    if ($line.Length -gt $cursor -and $line[$cursor] -eq $key.KeyChar) {
+        #Add another quote if next character is also a quote
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert($key.KeyChar)
+    }
+    else {
+        #Insert matching quotes, move cursor to be in between the quotes
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)" * 2)
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+        [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor - 1)
+    }
 }
 
 
