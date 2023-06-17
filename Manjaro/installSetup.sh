6bdbf46b9ce0d79646d3de225c9e6476fcb42ab2
shyguyCreate <107062289+shyguyCreate@users.noreply.github.com>
2023-06-17 12:23:07 -0600

    Update gists

--- Manjaro/installSetup.sh
+++ Manjaro/installSetup.sh
@@ -64,24 +64,19 @@ patch -sd $HOME < $HOME/Github/install-Scripts/share/.p10k.zsh.diff
 
 
 #Clone, install and add alias to update vscodium
-git clone https://gist.github.com/06679a4028cb574a946d026c713efa37.git $HOME/Github/gist/vscodiumInstaller
-source $HOME/Github/gist/vscodiumInstaller/vscodiumInstaller.sh
-echo 'alias codiumUpdate="source $HOME/Github/gist/vscodiumInstaller/vscodiumInstaller.sh"' >> $HOME/.zshrc
+git clone https://gist.github.com/06679a4028cb574a946d026c713efa37.git $HOME/Github/gist/vscodium-Installer
+source $HOME/Github/gist/vscodium-Installer/vscodium-Installer.sh
+echo 'alias codiumUpdate="source $HOME/Github/gist/vscodium-Installer/vscodium-Installer.sh"' >> $HOME/.zshrc
 
 #Clone, install and add alias to update pwsh
-git clone https://gist.github.com/86b8b157c90d6b2ebcb1eb98c4a701e8.git $HOME/Github/gist/pwshInstaller
-source $HOME/Github/gist/pwshInstaller/pwshInstaller.sh
-echo 'alias pwshUpdate="source $HOME/Github/gist/pwshInstaller/pwshInstaller.sh"' >> $HOME/.zshrc
+git clone https://gist.github.com/86b8b157c90d6b2ebcb1eb98c4a701e8.git $HOME/Github/gist/pwsh-Installer
+source $HOME/Github/gist/pwsh-Installer/pwsh-Installer.sh
+echo 'alias pwshUpdate="source $HOME/Github/gist/pwsh-Installer/pwsh-Installer.sh"' >> $HOME/.zshrc
 
 #Clone, install and add alias to update meslo-fonts
-git clone https://gist.github.com/3174d5463d717f7d7a8c67e45cd914be.git $HOME/Github/gist/meslofontsInstaller
-source $HOME/Github/gist/meslofontsInstaller/meslofontsInstaller.sh
-echo 'alias mesloUpdate="source $HOME/Github/gist/meslofontsInstaller/meslofontsInstaller.sh"' >> $HOME/.zshrc
-
-#Clone, install and add alias to update zoom
-git clone https://gist.github.com/fdec7db1dfe9588c0c3d735d142fcf41.git $HOME/Github/gist/zoomInstaller
-source $HOME/Github/gist/zoomInstaller/zoomInstaller.sh
-echo 'alias zoomUpdate="source $HOME/Github/gist/zoomInstaller/zoomInstaller.sh"' >> $HOME/.zshrc
+git clone https://gist.github.com/3174d5463d717f7d7a8c67e45cd914be.git $HOME/Github/gist/mesloLGS-Installer
+source $HOME/Github/gist/mesloLGS-Installer/mesloLGS-Installer.sh
+echo 'alias mesloUpdate="source $HOME/Github/gist/mesloLGS-Installer/mesloLGS-Installer.sh"' >> $HOME/.zshrc
 
 
 #Install Powershell modules
